86c7a01b96b0543ff364a3250b82ea2d570c7980
joka42 <jas.nombre@web.de>
2021-10-16 13:56:59 +0200

    added script

---
+++ cinnamon-high-dpi-switcher.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+switch_to_lowdpi_display () {
+	dconf write /org/cinnamon/desktop/interface/text-scaling-factor 1.0
+	dconf write /org/cinnamon/panels-height "['1:32']"
+	dconf write /org/cinnamon/panel-zone-icon-sizes "'[{\"panelId\":1, \"left\":24, \"center\":0, \"right\":16}]'"
+}
+
+switch_to_hidpi_display () {
+	dconf write /org/cinnamon/desktop/interface/text-scaling-factor 1.25
+	dconf write /org/cinnamon/panels-height "['1:48']"
+	dconf write /org/cinnamon/panel-zone-icon-sizes "'[{\"panelId\":1, \"left\":34, \"center\":0, \"right\":24}]'"
+}
+
